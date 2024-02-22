22974a8f216054a9d7beea7fc520f1ea147a5c11
MVP <you@example.com>
2024-02-21 21:43:54 -0500

    Updated movement. Added animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,21 @@ public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float rotateSpeed = 20f;
 
+    private bool isWalking;
+
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
+            inputVector.y += 1;
         }
         if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
+            inputVector.y -= 1;
         }
         if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
+            inputVector.x -= 1;
         }
         if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
+            inputVector.x += 1;
         }        
 
         inputVector = inputVector.normalized;
@@ -27,7 +29,14 @@ public class Player : MonoBehaviour {
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        isWalking = moveDir != Vector3.zero;
+        if (isWalking) {
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        }
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 
 }
