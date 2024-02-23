24bddd2e6cda6e3708eaf8e84b2ff5335c34f7a2
MVP <you@example.com>
2024-02-23 18:21:27 -0500

    Switch collision detection to capsule cast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,11 +15,13 @@ public class Player : MonoBehaviour {
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = 0.7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
         if (canMove) {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            transform.position += moveDir * moveDistance;
         }
 
         isWalking = moveDir != Vector3.zero;
