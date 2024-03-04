3d69a7d55d1173b0afedbb4b37b031b6a0bf3c25
MVP <you@example.com>
2024-03-03 22:05:19 -0500

    Add cut animation

---
+++ Assets/Scripts/CuttingCounterVisual.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounterVisual : MonoBehaviour {
+
+    private const string CUT = "Cut";
+
+    [SerializeField] private CuttingCounter cuttingCounter;
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+    }
+
+    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e) {
+        if (e.progressNormalized > 0) {
+            animator.SetTrigger(CUT);
+        }
+    }
+}
