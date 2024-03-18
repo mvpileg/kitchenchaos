08b606cb3b63a17262e1f57e51639e7775860f47
MVP <you@example.com>
2024-03-18 11:25:46 -0400

    Added textmesh pro. Started to build delivery manager UI

---
+++ Assets/Scripts/DeliveryManagerUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManagerUI : MonoBehaviour {
+
+    [SerializeField] private Transform container;
+    [SerializeField] private Transform recipeTemplate;
+
+    private void Awake() {
+        // 7:27:44
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
